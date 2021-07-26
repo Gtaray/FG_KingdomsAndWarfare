2947b655ad80adb6453faf44ae11b0a3c88149eb
Gtaray <gtaray@hotmail.com>
2021-07-26 15:53:07 -0700

    Finished Power Pool Updates
    Completed work on #15 , allowing dice to be dropped on a power pool.

--- scripts/manager_action_powerdie.lua
+++ scripts/manager_action_powerdie.lua
@@ -2,11 +2,8 @@
 -- Please see the license.html file included with this distribution for 
 -- attribution and copyright information.
 --
-local OOB_MSGTYPE_ADDPOWERDIE = "addpowerdie";
-local OOB_MSGTYPE_REMOVEPOWERDIE = "removepowerdie";
 
 function onInit()
-    OOBManager.registerOOBMsgHandler(OOB_MSGTYPE_REMOVEPOWERDIE, handleRemovePowerDie);
     ActionsManager.registerResultHandler("powerdie", onPowerDie)
 end
 
@@ -27,7 +24,7 @@ function getRoll(rActor, rAction)
         rRoll.sDesc = "[POWER DIE] Added";
     elseif rAction.remove then
         rRoll.aDice = { };
-        rRoll.nMod = rAction.nPowerdie;
+        rRoll.nMod = rAction.nMod;
         rRoll.sDesc = "[POWER DIE] Used";
     end
 	
@@ -48,57 +45,12 @@ function onPowerDie(rSource, rTarget, rRoll)
 
     if domainNode then
         if rRoll.sDesc:match("Added") then
-            notifyAddPowerDie(rSource, domainNode, nTotal);
+            PowerPoolManager.AddDieToPool(nTotal, domainNode);
         elseif rRoll.sDesc:match("Used") then
-            -- iterate on all of the power pool entries
-            local entries = DB.getChildren(node, "powerpool")
-            for k,v in pairs(entries) do
-                local value = DB.getValue(v, "value", 0);
-                if value == nTotal then
-                    -- Delete
-                    notifyRemovePowerDie(rSource, v);
-                    break;
-                end
-            end
+            PowerPoolManager.RemoveDieFromPool(nTotal, domainNode);
         end
     end
 
     rMessage.text = string.gsub(rMessage.text, "%[NODE:.+%]", "");
     Comm.deliverChatMessage(rMessage);    
-end
-
-function notifyAddPowerDie(rSource, domainNode, nTotal)
-    local msgOOB = {};
-	msgOOB.type = OOB_MSGTYPE_ADDPOWERDIE;
-	msgOOB.sDomain = domainNode.getNodeName();
-	msgOOB.sSourceNode = ActorManager.getCreatureNodeName(rSource);
-    msgOOB.nTotal = nTotal;
-
-	Comm.deliverOOBMessage(msgOOB, "");
-end
-
-function handleAddPowerDie(msgOOB)
-    local rSource = ActorManager.resolveActor(msgOOB.sSourceNode);
-    domainNode = DB.findNode(msgOOB.sDomain or "");
-    if domainNode then
-        local newDie = domainNode.createChild();
-        DB.setValue(newDie, "value", "number", msgOOB.nTotal or 0);
-    end
-end
-
-function notifyRemovePowerDie(rSource, domainNode)
-	local msgOOB = {};
-	msgOOB.type = OOB_MSGTYPE_REMOVEPOWERDIE;
-	msgOOB.sPowerDie = domainNode.getNodeName();
-	msgOOB.sSourceNode = ActorManager.getCreatureNodeName(rSource);
-
-	Comm.deliverOOBMessage(msgOOB, "");
-end
-
-function handleRemovePowerDie(msgOOB)
-    local rSource = ActorManager.resolveActor(msgOOB.sSourceNode);
-    local sNode = msgOOB.sPowerDie;
-    if sNode then
-        DB.deleteNode(sNode);
-    end
 end
