de7c63131069a16d36848727b1c06d3821397e4b
Gtaray <gtaray@hotmail.com>
2021-07-28 15:46:37 -0700

    Combat Tracker Fixes
    #23, #24, #25 Final testing for these issues

--- ct/scripts/ct_clientct.lua
+++ ct/scripts/ct_clientct.lua
@@ -3,17 +3,21 @@
 -- attribution and copyright information.
 --
 
+local fOnFilter;
+
 function onInit()
     if super and super.onInit then
         super.onInit();
     end
     
+    fOnFilter = list.onFilter;
     list.onFilter = onFilter;
+
+    list.applyFilter();
 end
 
 -- Update the player CT filter to take into account extra things
--- token visibility
--- faction
+-- token visibility & faction
 -- unit hidden property
 -- who was the last commander
 -- if the unit's commander is on the CT
@@ -22,32 +26,34 @@ function onFilter(w)
     local sFaction = w.friendfoe.getStringValue();
     local bTokenVisible = w.tokenvis.getValue() ~= 0;
 
-    -- If a token is hidden, it should 100% be hidden, no other concerns
-    if not bTokenVisible then
-        return false;
+    if ActorManagerKw.isUnit(node) then
+        -- If a token is hidden and not friendly, it should 100% be hidden, no other concerns
+        if not bTokenVisible then
+            if sFaction ~= "friend" then
+                return false;
+            end
+        end
+
+        -- Units without commanders should be visible as long as their token isn't hidden
+        local cmdrNode = ActorManagerKw.getCommanderCT(node);
+        if not cmdrNode then
+            return true;
+        end
+
+        -- Show units for the last non-unit actor on the combat tracker. 
+        local lastCommandersUnit = CombatManagerKw.isUnitOwnedByLastCommander(node);
+        if lastCommandersUnit then
+            return true;
+        end
+
+        -- Check if unit is manually hidden
+        local bHidden = DB.getValue(node, "hide", 0) == 1
+        if not bHidden then
+            return true;
+        end
+
+        return false
     end
 
-    -- Units without commanders should be visible as long as their token isn't hidden
-    local cmdrNode = ActorManagerKw.getCommanderCT(node);
-    if not cmdrNode then
-        return true;
-    end
-
-    -- Show units for the last non-unit actor on the combat tracker. 
-    local lastCommandersUnit = CombatManagerKw.isUnitOwnedByLastCommander(node);
-    if lastCommandersUnit then
-        return true;
-    end
-
-    -- Check if unit is manually hidden
-    if DB.getValue(node, "hide", 0) == 1 then
-        return false;
-    end
-
-    -- Lastly, check if the unit is friendly faction
-    if sFaction == "friend" then
-        return true;
-    end
-
-    return false
+    return fOnFilter(w);
 end
