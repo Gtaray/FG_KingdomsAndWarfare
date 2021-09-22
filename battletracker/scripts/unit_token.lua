a4b62c50f16da522da46f2bc5d5964755422a8a9
Shane Parker <shane.tim.parker@ea.com>
2021-09-22 13:31:55 -0400

    Added support for selecting to either top or bottom unit summary

--- battletracker/scripts/unit_token.lua
+++ battletracker/scripts/unit_token.lua
@@ -3,8 +3,70 @@
 -- attribution and copyright information.
 --
 
-function onDoubleClick(x, y)
-	window.windowlist.window.windowlist.window.active_unit.setValue("battletracker_unitsummary", window.getDatabaseNode().getPath());
+function onDrop(x, y, draginfo)
+	local sPrototype, dropref = draginfo.getTokenData();
+	if (sPrototype or "") == "" then
+		return nil;
+	end
+	
+	setPrototype(sPrototype);
+	CombatManager.replaceCombatantToken(window.getDatabaseNode(), dropref);
+	return true;
 end
 
--- todo drop handling ecosystem
+function onDragStart(draginfo)
+	local nSpace = DB.getValue(window.getDatabaseNode(), "space");
+	TokenManager.setDragTokenUnits(nSpace);
+end
+function onDragEnd(draginfo)
+	TokenManager.endDragTokenWithUnits();
+
+	local prototype, dropref = draginfo.getTokenData();
+	if dropref then
+		CombatManager.replaceCombatantToken(window.getDatabaseNode(), dropref);
+	end
+	return true;
+end
+
+function onClickDown(button, x, y)
+	return true;
+end
+function onClickRelease(button, x, y)
+	if button == 1 then
+		if Input.isControlPressed() then
+			local nodeActive = CombatManager.getActiveCT();
+			if nodeActive then
+				local nodeTarget = window.getDatabaseNode();
+				if nodeTarget then
+					TargetingManager.toggleCTTarget(nodeActive, nodeTarget);
+				end
+			end
+
+			--todo eww.
+			window.windowlist.window.windowlist.window.selected_unit.setValue("battletracker_unitsummary", window.getDatabaseNode().getPath());
+		elseif Input.isShiftPressed() then
+			--todo eww.
+			window.windowlist.window.windowlist.window.selected_unit.setValue("battletracker_unitsummary", window.getDatabaseNode().getPath());
+		else
+			local tokeninstance = CombatManager.getTokenFromCT(window.getDatabaseNode());
+			if tokeninstance and tokeninstance.isActivable() then
+				tokeninstance.setActive(not tokeninstance.isActive()); -- todo this wont work on clients... remove/relocate?
+			end
+
+			--todo eww.
+			window.windowlist.window.windowlist.window.active_unit.setValue("battletracker_unitsummary", window.getDatabaseNode().getPath());
+		end
+	end
+
+	return true;
+end
+
+function onDoubleClick(x, y)
+	CombatManager.openMap(window.getDatabaseNode());
+	-- unit activation if it is the commander's turn, or should control overloading be avoided here?
+end
+
+function onWheel(notches)
+	TokenManager.onWheelCT(window.getDatabaseNode(), notches);
+	return true;
+end
