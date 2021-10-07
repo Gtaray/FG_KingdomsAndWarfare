7fdb2df913288d8a1f561843fdc40d809aaf3d28
George Taray <gtaray@hotmail.com>
2021-10-07 13:39:21 -0700

    Added actions and effects lists to units, traits, and advantages
    Units, traits, and advantages now have new tabs that contain actions and effects that they should grant.
    
    Martial advantages will add have their powers added to PCs. Both advantages and traits have their unit effects added to units.
    
    To do:
    - When a unit is added to the CT, it should automatically add all effects in its list to itself.
    - When a trait or martial advantage is added to a unit in the CT, it should add all of the unit effects to that unit.

---
+++ common/scripts/actionslist.lua
@@ -0,0 +1,31 @@
+function addEntry(sType, bFocus)
+    local nodelist = getDatabaseNode();
+	if nodelist then
+        local nodeAction = nodelist.createChild();
+        if nodeAction then
+            DB.setValue(nodeAction, "type", "string", sType);
+        end
+	end
+end
+
+function reset()
+    for _,v in pairs(getWindows()) do
+        v.getDatabaseNode().delete();
+    end
+end
+
+function setOrder(node)
+    if DB.getValue(node, "order", 0) == 0 then
+        local aOrder = {};
+        for _,v in pairs(DB.getChildren(getDatabaseNode(), "")) do
+            aOrder[DB.getValue(v, "order", 0)] = true;
+        end
+        
+        local i = 1;
+        while aOrder[i] do
+            i = i + 1;
+        end
+        
+        DB.setValue(node, "order", "number", i);
+    end
+end
